b059e67de26f2e240b81a8ca205ef3c8aead5761
pendalph <pendalph@hotmail.com>
2019-08-24 00:38:18 +0300

    add skeleton

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import AccountServices from 'services/accountService';
+import Skeleton from 'react-loading-skeleton';
 
-const Player: React.FC<{id: number}> = () => {
+const Player: React.FC<{id: number}> = (props) => {
+
+    const [ info, setInfo ] = useState();
+
+    const wg = new AccountServices();
+
+    useEffect(() => {
+        wg.getAccountData(props.id)
+            .then((body) => {
+                setInfo(body.data)
+            });
+    }, []);
+
+    if(info) {
+        console.log('1')
+    }
+    else {
+        console.log('2')
+    }
+
     return(
-        <h1>player</h1>
+        <React.Fragment>
+            {info ? (<h1>{props.id}</h1>) : (<Skeleton count={5}/>)}
+        </React.Fragment>
     );
 };
 
--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -1,10 +1,10 @@
 class AccountServices {
     
-    public APP_ID: string = '#####################################'; // app keys - https://developers.wargaming.net/applications/
-    public API_URL: string = 'https://api.worldoftanks.ru/wot/account/list/?application_id';   // base link for receiving account information
+    public APP_ID: string = '3e5721b263115b6c375cce4e144c33ed'; // app keys - https://developers.wargaming.net/applications/
+    public API_URL: string = 'https://api.worldoftanks.ru/wot/account/info/?application_id';   // base link for receiving account information
 
-    async getAccountData(name: string){
-        const result = await fetch(`${this.API_URL}=${this.APP_ID}&search=${name}`);
+    async getAccountData(id: number){
+        const result = await fetch(`${this.API_URL}=${this.APP_ID}&account_id=${id}`);
 
         if (!result.ok) {
             throw new Error('Failed to get data.' + result.status);
