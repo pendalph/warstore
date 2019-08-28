39cf7a87d0737cdc44a3066016c2b0cd3f96d5f9
pendalph <pendalph@hotmail.com>
2019-08-28 21:59:48 +0300

    fix select value

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -49,41 +49,41 @@ const Player: React.FC<{id: number}> = (props) => {
                 <div>global_rating: {stats.global_rating}</div>
                 <div>nickname: {stats.nickname}</div>
                 <div>logout_at: {stats.logout_at}</div>
-                <div>spotted: {stats.statistics.all.spotted}</div>
-                <div>battles_on_stunning_vehicles: {stats.statistics.all.battles_on_stunning_vehicles}</div>
-                <div>max_xp: {stats.statistics.all.max_xp}</div>
-                <div>avg_damage_blocked: {stats.statistics.all.avg_damage_blocked}</div>
-                <div>direct_hits_received: {stats.statistics.all.direct_hits_received}</div>
-                <div>explosion_hits: {stats.statistics.all.explosion_hits}</div>
-                <div>piercings_received: {stats.statistics.all.piercings_received}</div>
-                <div>piercings: {stats.statistics.all.piercings}</div>
-                <div>max_damage_tank_id: {stats.statistics.all.max_damage_tank_id}</div>
-                <div>xp: {stats.statistics.all.xp}</div>
-                <div>survived_battles: {stats.statistics.all.survived_battles}</div>
-                <div>dropped_capture_points: {stats.statistics.all.dropped_capture_points}</div>
-                <div>hits_percents: {stats.statistics.all.hits_percents}</div>
-                <div>draws: {stats.statistics.all.draws}</div>
-                <div>max_xp_tank_id: {stats.statistics.all.max_xp_tank_id}</div>
-                <div>battles: {stats.statistics.all.battles}</div>
-                <div>damage_received: {stats.statistics.all.damage_received}</div>
-                <div>avg_damage_assisted: {stats.statistics.all.avg_damage_assisted}</div>
-                <div>max_frags_tank_id: {stats.statistics.all.max_frags_tank_id}</div>
-                <div>avg_damage_assisted_track: {stats.statistics.all.avg_damage_assisted_track}</div>
-                <div>frags: {stats.statistics.all.frags}</div>
-                <div>stun_number: {stats.statistics.all.stun_number}</div>
-                <div>avg_damage_assisted_radio: {stats.statistics.all.avg_damage_assisted_radio}</div>
-                <div>capture_points: {stats.statistics.all.capture_points}</div>
-                <div>stun_assisted_damage: {stats.statistics.all.stun_assisted_damage}</div>
-                <div>hits: {stats.statistics.all.hits}</div>
-                <div>battle_avg_xp: {stats.statistics.all.battle_avg_xp}</div>
-                <div>wins: {stats.statistics.all.wins}</div>
-                <div>losses: {stats.statistics.all.losses}</div>
-                <div>damage_dealt: {stats.statistics.all.damage_dealt}</div>
-                <div>no_damage_direct_hits_received: {stats.statistics.all.no_damage_direct_hits_received}</div>
-                <div>max_frags: {stats.statistics.all.max_frags}</div>
-                <div>shots: {stats.statistics.all.shots}</div>
-                <div>explosion_hits_received: {stats.statistics.all.explosion_hits_received}</div>
-                <div>tanking_factor: {stats.statistics.all.tanking_factor}</div>
+                <div>spotted: {stats.statistics[`${selectValue}`].spotted}</div>
+                <div>battles_on_stunning_vehicles: {stats.statistics[`${selectValue}`].battles_on_stunning_vehicles}</div>
+                <div>max_xp: {stats.statistics[`${selectValue}`].max_xp}</div>
+                <div>avg_damage_blocked: {stats.statistics[`${selectValue}`].avg_damage_blocked}</div>
+                <div>direct_hits_received: {stats.statistics[`${selectValue}`].direct_hits_received}</div>
+                <div>explosion_hits: {stats.statistics[`${selectValue}`].explosion_hits}</div>
+                <div>piercings_received: {stats.statistics[`${selectValue}`].piercings_received}</div>
+                <div>piercings: {stats.statistics[`${selectValue}`].piercings}</div>
+                <div>max_damage_tank_id: {stats.statistics[`${selectValue}`].max_damage_tank_id}</div>
+                <div>xp: {stats.statistics[`${selectValue}`].xp}</div>
+                <div>survived_battles: {stats.statistics[`${selectValue}`].survived_battles}</div>
+                <div>dropped_capture_points: {stats.statistics[`${selectValue}`].dropped_capture_points}</div>
+                <div>hits_percents: {stats.statistics[`${selectValue}`].hits_percents}</div>
+                <div>draws: {stats.statistics[`${selectValue}`].draws}</div>
+                <div>max_xp_tank_id: {stats.statistics[`${selectValue}`].max_xp_tank_id}</div>
+                <div>battles: {stats.statistics[`${selectValue}`].battles}</div>
+                <div>damage_received: {stats.statistics[`${selectValue}`].damage_received}</div>
+                <div>avg_damage_assisted: {stats.statistics[`${selectValue}`].avg_damage_assisted}</div>
+                <div>max_frags_tank_id: {stats.statistics[`${selectValue}`].max_frags_tank_id}</div>
+                <div>avg_damage_assisted_track: {stats.statistics[`${selectValue}`].avg_damage_assisted_track}</div>
+                <div>frags: {stats.statistics[`${selectValue}`].frags}</div>
+                <div>stun_number: {stats.statistics[`${selectValue}`].stun_number}</div>
+                <div>avg_damage_assisted_radio: {stats.statistics[`${selectValue}`].avg_damage_assisted_radio}</div>
+                <div>capture_points: {stats.statistics[`${selectValue}`].capture_points}</div>
+                <div>stun_assisted_damage: {stats.statistics[`${selectValue}`].stun_assisted_damage}</div>
+                <div>hits: {stats.statistics[`${selectValue}`].hits}</div>
+                <div>battle_avg_xp: {stats.statistics[`${selectValue}`].battle_avg_xp}</div>
+                <div>wins: {stats.statistics[`${selectValue}`].wins}</div>
+                <div>losses: {stats.statistics[`${selectValue}`].losses}</div>
+                <div>damage_dealt: {stats.statistics[`${selectValue}`].damage_dealt}</div>
+                <div>no_damage_direct_hits_received: {stats.statistics[`${selectValue}`].no_damage_direct_hits_received}</div>
+                <div>max_frags: {stats.statistics[`${selectValue}`].max_frags}</div>
+                <div>shots: {stats.statistics[`${selectValue}`].shots}</div>
+                <div>explosion_hits_received: {stats.statistics[`${selectValue}`].explosion_hits_received}</div>
+                <div>tanking_factor: {stats.statistics[`${selectValue}`].tanking_factor}</div>
             </React.Fragment>
         )
     }
