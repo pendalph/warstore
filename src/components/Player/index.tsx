9faa00bfc949cdd6320bd8093bfde760bfb90e27
pendalph <pendalph@hotmail.com>
2019-08-29 13:42:38 +0300

    add description player data

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -25,7 +25,11 @@ const Player: React.FC<{id: number}> = (props) => {
     }
 
     const PlayerData = () => {
+
         const stats = info[props.id];
+        const lastBattleTime = new Date(stats.last_battle_time * 1000).toString();
+        const createdAt = new Date(stats.created_at * 1000).toString();
+
         return (
             <React.Fragment>
                 <form onSubmit={handleSubmit}>
@@ -43,47 +47,46 @@ const Player: React.FC<{id: number}> = (props) => {
                         </select>
                     </label>
                 </form>
-                <div>last_battle_time: {stats.last_battle_time}</div>
-                <div>created_at: {stats.created_at}</div>
-                <div>updated_at: {stats.updated_at}</div>
-                <div>global_rating: {stats.global_rating}</div>
-                <div>nickname: {stats.nickname}</div>
-                <div>logout_at: {stats.logout_at}</div>
-                <div>spotted: {stats.statistics[selectValue].spotted}</div>
-                <div>battles_on_stunning_vehicles: {stats.statistics[selectValue].battles_on_stunning_vehicles}</div>
-                <div>max_xp: {stats.statistics[selectValue].max_xp}</div>
-                <div>avg_damage_blocked: {stats.statistics[selectValue].avg_damage_blocked}</div>
-                <div>direct_hits_received: {stats.statistics[selectValue].direct_hits_received}</div>
-                <div>explosion_hits: {stats.statistics[selectValue].explosion_hits}</div>
-                <div>piercings_received: {stats.statistics[selectValue].piercings_received}</div>
-                <div>piercings: {stats.statistics[selectValue].piercings}</div>
-                <div>max_damage_tank_id: {stats.statistics[selectValue].max_damage_tank_id}</div>
-                <div>xp: {stats.statistics[selectValue].xp}</div>
-                <div>survived_battles: {stats.statistics[selectValue].survived_battles}</div>
-                <div>dropped_capture_points: {stats.statistics[selectValue].dropped_capture_points}</div>
-                <div>hits_percents: {stats.statistics[selectValue].hits_percents}</div>
-                <div>draws: {stats.statistics[selectValue].draws}</div>
-                <div>max_xp_tank_id: {stats.statistics[selectValue].max_xp_tank_id}</div>
-                <div>battles: {stats.statistics[selectValue].battles}</div>
-                <div>damage_received: {stats.statistics[selectValue].damage_received}</div>
-                <div>avg_damage_assisted: {stats.statistics[selectValue].avg_damage_assisted}</div>
-                <div>max_frags_tank_id: {stats.statistics[selectValue].max_frags_tank_id}</div>
-                <div>avg_damage_assisted_track: {stats.statistics[selectValue].avg_damage_assisted_track}</div>
-                <div>frags: {stats.statistics[selectValue].frags}</div>
-                <div>stun_number: {stats.statistics[selectValue].stun_number}</div>
-                <div>avg_damage_assisted_radio: {stats.statistics[selectValue].avg_damage_assisted_radio}</div>
-                <div>capture_points: {stats.statistics[selectValue].capture_points}</div>
-                <div>stun_assisted_damage: {stats.statistics[selectValue].stun_assisted_damage}</div>
-                <div>hits: {stats.statistics[selectValue].hits}</div>
-                <div>battle_avg_xp: {stats.statistics[selectValue].battle_avg_xp}</div>
-                <div>wins: {stats.statistics[selectValue].wins}</div>
-                <div>losses: {stats.statistics[selectValue].losses}</div>
-                <div>damage_dealt: {stats.statistics[selectValue].damage_dealt}</div>
-                <div>no_damage_direct_hits_received: {stats.statistics[selectValue].no_damage_direct_hits_received}</div>
-                <div>max_frags: {stats.statistics[selectValue].max_frags}</div>
-                <div>shots: {stats.statistics[selectValue].shots}</div>
-                <div>explosion_hits_received: {stats.statistics[selectValue].explosion_hits_received}</div>
-                <div>tanking_factor: {stats.statistics[selectValue].tanking_factor}</div>
+                <div>Время последнего боя: {lastBattleTime}</div>
+                <div>Аккаунт создан: {createdAt}</div>
+                <div>Личный рейтинг: {stats.global_rating}</div>
+                <div>Имя пользователя: {stats.nickname}</div>
+                <div>Время окончания последней игровой сессии: {stats.logout_at}</div>
+                <div>Обнаружено противников: {stats.statistics[selectValue].spotted}</div>
+                <div>Количество боёв на технике, причиняющей оглушение: {stats.statistics[selectValue].battles_on_stunning_vehicles}</div>
+                <div>Максимальный опыт за бой: {stats.statistics[selectValue].max_xp}</div>
+                <div>Средний заблокированный бронёй урон за бой: {stats.statistics[selectValue].avg_damage_blocked}</div>
+                <div>Количество полученных прямых попаданий: {stats.statistics[selectValue].direct_hits_received}</div>
+                <div>Количество нанесённых осколочно-фугасных попаданий: {stats.statistics[selectValue].explosion_hits}</div>
+                <div>Количество полученных пробитий: {stats.statistics[selectValue].piercings_received}</div>
+                <div>Количество пробитий: {stats.statistics[selectValue].piercings}</div>
+                <div>Техника, на которой был нанесён максимальный урон за бой: {stats.statistics[selectValue].max_damage_tank_id}</div>
+                <div>Максимальный опыт за бой: {stats.statistics[selectValue].xp}</div>
+                <div>Выжил в боях: {stats.statistics[selectValue].survived_battles}</div>
+                <div>Очки защиты базы: {stats.statistics[selectValue].dropped_capture_points}</div>
+                <div>Процент попаданий: {stats.statistics[selectValue].hits_percents}</div>
+                <div>Ничьи: {stats.statistics[selectValue].draws}</div>
+                <div>Техника, на которой получен максимальный опыт за бой: {stats.statistics[selectValue].max_xp_tank_id}</div>
+                <div>Выжил в боях: {stats.statistics[selectValue].battles}</div>
+                <div>Получено урона: {stats.statistics[selectValue].damage_received}</div>
+                <div>Средний урон, нанесённый с вашей помощью: {stats.statistics[selectValue].avg_damage_assisted}</div>
+                <div>Техника, на которой уничтожено максимальное количество противников за бой: {stats.statistics[selectValue].max_frags_tank_id}</div>
+                <div>Средний урон после вашего попадания, сбившего гусеницу: {stats.statistics[selectValue].avg_damage_assisted_track}</div>
+                <div>Уничтожено техники: {stats.statistics[selectValue].frags}</div>
+                <div>Количество оглушений, причинённых экипажу противника: {stats.statistics[selectValue].stun_number}</div>
+                <div>Средний урон по вашим разведданным: {stats.statistics[selectValue].avg_damage_assisted_radio}</div>
+                <div>Очки захвата базы: {stats.statistics[selectValue].capture_points}</div>
+                <div>Урон по оглушённым вами целям: {stats.statistics[selectValue].stun_assisted_damage}</div>
+                <div>Попадания: {stats.statistics[selectValue].hits}</div>
+                <div>Средний опыт за бой: {stats.statistics[selectValue].battle_avg_xp}</div>
+                <div>Победы: {stats.statistics[selectValue].wins}</div>
+                <div>Поражения: {stats.statistics[selectValue].losses}</div>
+                <div>Нанесено повреждений: {stats.statistics[selectValue].damage_dealt}</div>
+                <div>Количество полученных прямых попаданий, не нанёсших урон: {stats.statistics[selectValue].no_damage_direct_hits_received}</div>
+                <div>Максимум уничтожено за бой: {stats.statistics[selectValue].max_frags}</div>
+                <div>Произведено выстрелов: {stats.statistics[selectValue].shots}</div>
+                <div>Количество полученных осколочно-фугасных попаданий: {stats.statistics[selectValue].explosion_hits_received}</div>
+                <div>Отношение заблокированного бронёй урона к полученному игроком урону от бронебойных, кумулятивных, подкалиберных снарядов: {stats.statistics[selectValue].tanking_factor}</div>
             </React.Fragment>
         )
     }
